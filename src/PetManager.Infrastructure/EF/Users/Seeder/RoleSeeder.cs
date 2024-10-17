e6e904c8875c0d6a737c4f23ff43179d08227b3f
Justyna <justyna.kurcz1@gmail.com>
2024-10-17 21:00:51 +0200

    Add: role and admin user seeder

---
+++ src/PetManager.Infrastructure/EF/Users/Seeder/RoleSeeder.cs
@@ -0,0 +1,35 @@
+using PetManager.Core.Users.Entities;
+using PetManager.Infrastructure.EF.Context;
+
+namespace PetManager.Infrastructure.EF.Users.Seeder;
+
+public class RoleSeeder(PetManagerDbContext dbContext)
+{
+    public void Seed()
+    {
+        var roles = new[]
+        {
+            Role.Create("Admin"),
+            Role.Create("User")
+        };
+
+        foreach (var role in roles)
+        {
+            if (!dbContext.Roles.Any(r => r.Name == role.Name))
+            {
+                dbContext.Roles.Add(role);
+            }
+        }
+
+        dbContext.SaveChanges();
+
+        if (!dbContext.Users.Any(u => u.Email == "admin@petmanager.com"))
+        {
+            var adminRoleId = dbContext.Roles.First(r => r.Name == "Admin").RoleId;
+            var adminUser = User.Create("admin@petmanager.com", "!23Haslo", adminRoleId);
+            dbContext.Users.Add(adminUser);
+        }
+
+        dbContext.SaveChanges();
+    }
+}
--- src/PetManager.Infrastructure/Extensions.cs
+++ src/PetManager.Infrastructure/Extensions.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PetManager.Core.Users.Repositories;
 using PetManager.Infrastructure.EF.Context;
 using PetManager.Infrastructure.EF.Users.Repositories;
+using PetManager.Infrastructure.EF.Users.Seeder;
 using PetManager.Infrastructure.Exceptions;
 
 namespace PetManager.Infrastructure;
@@ -22,7 +24,19 @@ public static class Extensions
             .AddScoped<IUserRepository, UserRepository>()
             .AddScoped<IRoleRepository, RoleRepository>();
 
+        services.AddScoped<RoleSeeder>();
 
         return services;
+    }
+
+    public static IApplicationBuilder UseSeeder(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<PetManagerDbContext>();
+
+        var roleSeeder = new RoleSeeder(dbContext);
+        roleSeeder.Seed();
+
+        return app;
     }
 }
